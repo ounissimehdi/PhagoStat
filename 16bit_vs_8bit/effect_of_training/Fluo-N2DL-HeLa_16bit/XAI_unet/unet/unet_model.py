5675f616b405ce22257a567b691d1b9b99223255
Mehdi Ounissi <95075720+ounissimehdi@users.noreply.github.com>
2024-01-15 11:33:38 +0100

    8bit vs 16bit

---
+++ 16bit_vs_8bit/effect_of_training/Fluo-N2DL-HeLa_16bit/XAI_unet/unet/unet_model.py
@@ -0,0 +1,213 @@
+# ------------------------------------------------------------------------------
+#
+#
+#                                 P̶̖̥͈͈͇̼͇̈́̈́̀͋͒̽͊͘͠h̴͙͈̦͗́̓a̴̧͗̾̀̅̇ḡ̶͓̭̝͓͎̰͓̦̭̎́͊̐̂͒͠ơ̵̘͌̿̽̑̈̾Ś̴̠́̓̋̂̃͒͆̚t̴̲͓̬͎̾͑͆͊́̕a̸͍̫͎̗̞͆̇̀̌̑͂t̸̳̼̘͕̯̠̱̠͂̔̒̐̒̕͝͝
+#
+#
+#                                PhagoStat
+#                Advanced Phagocytic Activity Analysis Tool
+# ------------------------------------------------------------------------------
+# Copyright (C) 2023 Mehdi OUNISSI <mehdi.ounissi@icm-institute.org>
+#               Sorbonne University, Paris Brain Institute - ICM, CNRS, Inria,
+#               Inserm, AP-HP, Paris, 75013, France.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+# Note on Imported Packages:
+# The packages used in this work are imported as is and not modified. If you
+# intend to use, modify, or distribute any of these packages, please refer to
+# the requirements.txt file and the respective package licenses.
+# ------------------------------------------------------------------------------
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Unet(nn.Module):
+    def __init__(self, n_channels, n_classes, conv_num=24):
+        super(Unet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+
+        self.inc = DoubleConv(n_channels, conv_num)
+        self.down1 = Down(conv_num, conv_num*2)
+        self.down2 = Down(conv_num*2, conv_num*4)
+        self.down3 = Down(conv_num*4, conv_num*8)
+        self.down4 = Down(conv_num*8, conv_num*16)
+
+        self.mid = DoubleConv(conv_num*16, conv_num*16)
+
+        self.up1 = Up(conv_num*16, conv_num*8)
+        self.up2 = Up(conv_num*8, conv_num*4)
+        self.up3 = Up(conv_num*4, conv_num*2)
+        self.up4 = Up(conv_num*2, conv_num)
+        self.outc = OutConv(conv_num, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        x5 = self.mid(x5)
+
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+
+        logits = self.outc(x)
+        return logits
+    
+class VisUnet(nn.Module):
+    def __init__(self, n_channels, n_classes, conv_num=24):
+        super(VisUnet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+
+        self.inc = DoubleConv(n_channels, conv_num)
+        self.down1 = Down(conv_num, conv_num*2)
+        self.down2 = Down(conv_num*2, conv_num*4)
+        self.down3 = Down(conv_num*4, conv_num*8)
+        self.down4 = Down(conv_num*8, conv_num*16)
+
+        self.mid = DoubleConv(conv_num*16, conv_num*16)
+
+        self.up1 = Up(conv_num*16, conv_num*8)
+        self.up2 = Up(conv_num*8, conv_num*4)
+        self.up3 = Up(conv_num*4, conv_num*2)
+        self.up4 = Up(conv_num*2, conv_num)
+        self.outc = OutConv(conv_num, n_classes)
+
+    def forward(self, x):
+
+        x1 = self.inc(x)
+        
+        x2 = self.down1(x1)
+        mean_x2 = x2.mean(dim=1).detach().cpu().squeeze().numpy()
+
+        x3 = self.down2(x2)
+        mean_x3 = x3.mean(dim=1).detach().cpu().squeeze().numpy()
+
+        x4 = self.down3(x3)
+        mean_x4 = x4.mean(dim=1).detach().cpu().squeeze().numpy()
+
+        x5 = self.down4(x4)
+        mean_x5 = x5.mean(dim=1).detach().cpu().squeeze().numpy()
+
+        x_mid = self.mid(x5)
+        mean_x_mid = x_mid.mean(dim=1).detach().cpu().squeeze().numpy()
+
+        x = self.up1(x_mid, x4)
+        mean_x_6 = x.mean(dim=1).detach().cpu().squeeze().numpy()
+
+        x = self.up2(x, x3)
+        mean_x_7 = x.mean(dim=1).detach().cpu().squeeze().numpy()
+
+        x = self.up3(x, x2)
+        mean_x_8 = x.mean(dim=1).detach().cpu().squeeze().numpy()
+
+        x = self.up4(x, x1)
+        mean_x_9 = x.mean(dim=1).detach().cpu().squeeze().numpy()
+
+        logits = self.outc(x)
+        
+        mean_values = [mean_x2, mean_x3, mean_x4, mean_x5, mean_x_mid, mean_x_6, mean_x_7, mean_x_8, mean_x_9]
+
+        return mean_values, logits
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.LeakyReLU(inplace=False),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(inplace=False),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+def test_model():
+    x = torch.randn((1,1,2048,2048))
+    model = Unet(n_channels=1, n_classes=1)
+    print(model)
+
+    pytorch_total_params = sum(p.numel() for p in model.parameters())
+    print(pytorch_total_params)
+    # Apply sigmoid in case on MSE loss
+    preds = model(x)
+    preds = torch.sigmoid(preds)
+    print(f'input shape: {x.shape}')
+    print(f'output shape: {preds.shape}')
+
+if __name__ == "__main__":
+    test_model()
